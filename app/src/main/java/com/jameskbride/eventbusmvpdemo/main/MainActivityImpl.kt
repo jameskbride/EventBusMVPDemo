0ca34af3d3e7375137b9c4608fe82d0d6d5c0ed3
Jim Kirkbride <james.kirkbride@gmail.com>
2017-12-31 12:08:05 -0500

    Adding orders block.

--- app/src/main/java/com/jameskbride/eventbusmvpdemo/main/MainActivityImpl.kt
+++ app/src/main/java/com/jameskbride/eventbusmvpdemo/main/MainActivityImpl.kt
@@ -1,10 +1,13 @@
 package com.jameskbride.eventbusmvpdemo.main
 
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import com.jameskbride.eventbusmvpdemo.R
 import com.jameskbride.eventbusmvpdemo.network.BurritosToGoApi
+import com.jameskbride.eventbusmvpdemo.network.Order
 import com.jameskbride.eventbusmvpdemo.network.ProfileResponse
 import com.jameskbride.eventbusmvpdemo.utils.ToasterWrapper
 import retrofit2.Call
@@ -27,15 +30,29 @@ class MainActivityImpl @Inject constructor(val burritosToGoApi: BurritosToGoApi,
             }
 
             override fun onResponse(call: Call<ProfileResponse>?, response: Response<ProfileResponse>?) {
-                mainActivity.findViewById<TextView>(R.id.customer_name).text = "${response?.body()?.firstName} ${response?.body()?.lastName}"
-                mainActivity.findViewById<TextView>(R.id.address_line_1).text = response?.body()?.addressLine1
-                mainActivity.findViewById<TextView>(R.id.address_line_2).text = response?.body()?.addressLine2
-                mainActivity.findViewById<TextView>(R.id.city).text = response?.body()?.city
-                mainActivity.findViewById<TextView>(R.id.state).text = response?.body()?.state
-                mainActivity.findViewById<TextView>(R.id.zipcode).text = response?.body()?.zipCode
+                setProfileDetails(mainActivity, response)
+
+                showOrders(response?.body()?.orderHistory, mainActivity)
             }
         })
     }
 
+    private fun setProfileDetails(mainActivity: MainActivity, response: Response<ProfileResponse>?) {
+        mainActivity.findViewById<TextView>(R.id.customer_name).text = "${response?.body()?.firstName} ${response?.body()?.lastName}"
+        mainActivity.findViewById<TextView>(R.id.address_line_1).text = response?.body()?.addressLine1
+        mainActivity.findViewById<TextView>(R.id.address_line_2).text = response?.body()?.addressLine2
+        mainActivity.findViewById<TextView>(R.id.city).text = response?.body()?.city
+        mainActivity.findViewById<TextView>(R.id.state).text = response?.body()?.state
+        mainActivity.findViewById<TextView>(R.id.zipcode).text = response?.body()?.zipCode
+    }
 
+    private fun showOrders(orderHistory:List<Order>?, mainActivity: MainActivity) {
+        if (orderHistory?.isNotEmpty()!!) {
+            mainActivity.findViewById<LinearLayout>(R.id.no_orders_block).visibility = View.VISIBLE
+            mainActivity.findViewById<LinearLayout>(R.id.found_orders_block).visibility = View.GONE
+        } else {
+            mainActivity.findViewById<LinearLayout>(R.id.no_orders_block).visibility = View.GONE
+            mainActivity.findViewById<LinearLayout>(R.id.found_orders_block).visibility = View.VISIBLE
+        }
+    }
 }
