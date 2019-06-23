4e06e48abae943986a2a6c742350fc6e484bd575
Jim Kirkbride <james.kirkbride@gmail.com>
2019-06-23 13:50:36 -0400

    Using the updated androidx dependencies for espresso.

--- app/src/androidTest/java/com/jameskbride/eventbusmvpdemo/ProfilePageTest.kt
+++ app/src/androidTest/java/com/jameskbride/eventbusmvpdemo/ProfilePageTest.kt
@@ -1,10 +1,10 @@
 package com.jameskbride.eventbusmvpdemo
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.*
-import android.support.test.rule.ActivityTestRule
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
 import clickOn
 import com.jameskbride.eventbusmvpdemo.main.MainActivity
 import org.hamcrest.core.AllOf.allOf
--- app/src/androidTest/java/helpers/EspressoHelpers.kt
+++ app/src/androidTest/java/helpers/EspressoHelpers.kt
@@ -1,9 +1,9 @@
-import android.support.test.espresso.Espresso
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.*
-import android.support.test.espresso.assertion.ViewAssertions
-import android.support.test.espresso.matcher.ViewMatchers.*
 import android.util.Log
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers.*
 
 fun waitOn(millis: Long = 10000, action: () -> Unit) {
     var millisLeft = millis
