a75e29101bfb8b9bdf4391cb05cb2d4ffd509199
Jim Kirkbride <james.kirkbride@gmail.com>
2019-06-23 07:08:28 -0400

    Introducing an Espresso test.

---
+++ app/src/androidTest/java/com/jameskbride/eventbusmvpdemo/ProfilePageTest.kt
@@ -0,0 +1,24 @@
+package com.jameskbride.eventbusmvpdemo
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import com.jameskbride.eventbusmvpdemo.main.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ProfilePageTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java,true, true)
+
+    @Test
+    fun whenTheViewLoadsItDisplaysTheSearchView() {
+        onView(withId(R.id.profile_id_edit)).check(matches(isDisplayed()));
+    }
+}
